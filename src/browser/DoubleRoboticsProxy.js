8f6bd504d8ccfc9ff24bed22a94cab7b49bba94e
Jakob Engelbrecht Olesen <jakob@basher.dk>
2016-03-10 15:35:05 +0100

    Added browser platform (DoubleRoboticsProxy)

---
+++ src/browser/DoubleRoboticsProxy.js
@@ -0,0 +1,14 @@
+﻿function stop() {
+    console.log("DoubleRoboticsProxy.stop()");
+}
+
+module.exports = {
+    stop: function (success, error) {
+        stop();
+        setTimeout(function () {
+            success();
+        }, 0);
+    }
+};
+
+require("cordova/exec/proxy").add("DoubleRobotics", module.exports);
