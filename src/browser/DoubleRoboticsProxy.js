1e23df8ce3d088fcd461dc6ac5deda6a336c6204
Jakob Engelbrecht Olesen <jakob@basher.dk>
2016-04-20 12:54:23 +0200

    Added battery status to proxy + replaced drive data with speed

--- src/browser/DoubleRoboticsProxy.js
+++ src/browser/DoubleRoboticsProxy.js
@@ -2,9 +2,10 @@ function DoubleRoboticsProxy() {
     var travelDataCallback,
         collisionCallback,
         driveIntervalId,
+        batteryStatusIntervalId,
         leftEncoderTotalCm,
         rightEncoderTotalCm,
-        driveData = [],
+        avgEncoderTotalCm,
         driveStartDate;
 
     this.pole = function (success, fail, arguments) {
@@ -43,10 +44,13 @@ function DoubleRoboticsProxy() {
         var driveCounter = 0;
 
         if (typeof driveDirection === "string") driveDirection = parseFloat(driveDirection);
+        var currentDrive = driveDirection,
+            currentSpeed = 0;
         //if (!driveStartDate) {
         driveStartDate = new Date();
         leftEncoderTotalCm = 0.0;
         rightEncoderTotalCm = 0.0;
+        avgEncoderTotalCm = 0.0;
         //}
         //console.log("DoubleRoboticsProxy.drive | driveDirection:" + driveDirection);// + " (" + new Date().toISOString() + ")");
         if (driveIntervalId) {
@@ -54,30 +58,40 @@ function DoubleRoboticsProxy() {
         }
         driveIntervalId = setInterval(function () {
             driveCounter++;
+            var accelerateEnd = 50;
             var deaccelerateStart = 50;
-            var remainingRange = rangeInCm - Math.abs(leftEncoderTotalCm);
-            if (rangeInCm && remainingRange < deaccelerateStart) {
-                driveDirection *= 0.95;
+            var range = Math.abs(avgEncoderTotalCm);
+            var remainingRange = rangeInCm - range;
+            if (range < accelerateEnd) {
+              currentSpeed += (10.0 * driveDirection);
             }
-            leftEncoderTotalCm += (10.0 * driveDirection);
-            rightEncoderTotalCm += (10.0 * driveDirection);
+            if (rangeInCm > 0) {
+              if (remainingRange < deaccelerateStart) {
+                currentDrive *= 0.95;
+                currentSpeed -= (7.5 * driveDirection);
+              }
+            }
+            leftEncoderTotalCm += (10.0 * currentDrive);
+            rightEncoderTotalCm += (10.0 * currentDrive);
+            avgEncoderTotalCm = (leftEncoderTotalCm + rightEncoderTotalCm) / 2;
+            range = Math.abs(avgEncoderTotalCm);
+            if (currentSpeed < 0 || (range >= rangeInCm)) {
+              currentSpeed = 0.0;
+            }
             var elapsedTimeInMs = new Date() - driveStartDate;
-            var newData = {
-                speed: driveDirection,
-                range: Math.abs(leftEncoderTotalCm),
-                time: new Date(),//msToTime(elapsedTimeInMs),
-                start: driveStartDate
-            };
-            driveData.push(newData)
-            //console.log("DoubleRoboticsProxy.drive | driveDirection:" + driveDirection + " | leftEncoderTotalCm: " + leftEncoderTotalCm + " | elapsedTimeInMs: " + elapsedTimeInMs);// + " (" + new Date().toISOString() + ")");
+            //console.log("DoubleRoboticsProxy.drive | driveCounter: " + driveCounter + " | currentDrive:" + currentDrive + " | currentSpeed: " + currentSpeed + " | range: " + range + " | elapsedTimeInMs: " + elapsedTimeInMs);// + " (" + new Date().toISOString() + ")");
             var cmPerInches = 2.54;
             var travelData = {
                 leftEncoderTotalInches: leftEncoderTotalCm / cmPerInches,
                 rightEncoderTotalInches: rightEncoderTotalCm / cmPerInches,
+                avgEncoderTotalInches: avgEncoderTotalCm / cmPerInches,
                 leftEncoderTotalCm: leftEncoderTotalCm,
                 rightEncoderTotalCm: rightEncoderTotalCm,
-                driveData: driveData,
-                lastDrive: newData
+                avgEncoderTotalCm: avgEncoderTotalCm,
+                speed: currentSpeed,
+                range: range,
+                time: new Date(),//msToTime(elapsedTimeInMs),
+                start: driveStartDate
             };
             if (driveCounter === 10) {
               if (collisionCallback && typeof collisionCallback === "function") {
@@ -89,12 +103,12 @@ function DoubleRoboticsProxy() {
             }
             if (travelDataCallback && typeof travelDataCallback === "function")
                 travelDataCallback(travelData);
-            if (rangeInCm && Math.abs(leftEncoderTotalCm) >= rangeInCm) {
-                console.log("DoubleRoboticsProxy.stop => rangeInCm >= " + rangeInCm + " | leftEncoderTotalCm: " + leftEncoderTotalCm);// + " (" + new Date
+            if (rangeInCm && range >= rangeInCm) {
+                console.log("DoubleRoboticsProxy.stop => rangeInCm >= " + rangeInCm + " | range: " + range);// + " (" + new Date
                 clearInterval(driveIntervalId);
                 if (rangeInCm && rangeInCm > 0) {
                   if (typeof success === "function") {
-                    success({ serial: "00-00FAKE", message: "Fake Robot..." });
+                    success(travelData);
                   }
                 }
 
@@ -103,7 +117,7 @@ function DoubleRoboticsProxy() {
         if (!rangeInCm || rangeInCm === 0) {
           if (typeof success === "function") {
               setTimeout(function () {
-                  success({ serial: "00-00FAKE", message: "Fake Robot..." });
+                  success();
               }, 0);
           }
         }
@@ -115,16 +129,43 @@ function DoubleRoboticsProxy() {
     this.stop = function (success, fail) {
         console.log("DoubleRoboticsProxy.stop()");
         clearInterval(driveIntervalId);
-        //driveData.length = 0;
         //driveStartDate = null;
     }
 
     this.startStatusListener = function (success, fail) {
         console.log("DoubleRoboticsProxy.startStatusListener()");
+        var batteryPercent = 100;
+        var status = {
+          "batteryPercent" : batteryPercent / 100.0,
+          "batteryIsFullyCharged" : true,
+          "kickstandState" : 0,
+          "poleHeightPercent" : 0,
+          "serial" : '00-00FAKE',
+          "firmwareVersion" : '1004'
+         };
+        if (typeof success === "function") {
+            setTimeout(function () {
+                console.log("DoubleRoboticsProxy.batteryStatus: " + status.batteryPercent);
+                success(status);
+            }, 0);
+            batteryStatusIntervalId = setInterval(function() {
+              batteryPercent -= 1;
+              status.batteryPercent = batteryPercent / 100.0;
+              status.batteryIsFullyCharged = (batteryPercent === 100);
+              console.log("DoubleRoboticsProxy.batteryStatus: " + status.batteryPercent);
+              if (status.batteryPercent >= 0) {
+                success(status);
+              }
+              if (status.batteryPercent <= 0) {
+                clearInterval(batteryStatusIntervalId);
+              }
+            }, 10000);
+        }
     }
 
     this.stopStatusListener = function (success, fail) {
         console.log("DoubleRoboticsProxy.stopStatusListener()");
+        clearInterval(batteryStatusIntervalId);
     }
 
     this.startTravelDataListener = function (success, fail) {
@@ -144,7 +185,7 @@ function DoubleRoboticsProxy() {
 
     this.stopCollisionListener = function (success, fail) {
         console.log("DoubleRoboticsProxy.stopCollisionListener()");
-        travelDataCallback = null;
+        collisionCallback = null;
     }
 
     function msToTime(duration) {
